d793fffbc8a817fd1679b23f875481aae0273f77
dev-nicolaos <contact@nicolaos.dev>
2025-02-11 10:32:43 -0800

    Passing Spanish support

--- hello_test.go
+++ hello_test.go
@@ -11,14 +11,20 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 
 func TestHello(t *testing.T) {
 	t.Run("Say hello to name", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say hello to world if name is empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Juan", "Spanish")
+		want := "Hola Juan"
 
 		assertCorrectMessage(t, got, want)
 	})
