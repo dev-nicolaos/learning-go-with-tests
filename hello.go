727f91483c0647e34ecded9de0ec88a1cd507df4
dev-nicolaos <contact@nicolaos.dev>
2025-02-11 10:08:39 -0800

    Failing Hello Name

--- hello.go
+++ hello.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func Hello() string {
+func Hello(name string) string {
 	return "Hello World"
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("World"))
 }
