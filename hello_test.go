92419823b4b4a27df34821d606adcdfa7daf0ce5
dev-nicolaos <contact@nicolaos.dev>
2025-02-11 10:18:40 -0800

    Failing Hello Name fallback

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,24 @@ package main
 
 import "testing"
 
+const basicFailTemplate = "got %q want %q"
+
+
 func TestHello(t *testing.T) {
+	t.Run("Say hello to name", func(t *testing.T) {
 	got := Hello("Chris")
 	want := "Hello Chris"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf(basicFailTemplate, got, want)
 	}
+	})
+	t.Run("Say hello to world if name is empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		if got != want {
+			t.Errorf(basicFailTemplate, got, want)
+		}
+	})
 }
