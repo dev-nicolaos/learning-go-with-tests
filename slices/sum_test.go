3a6aeb981afc18b12ad5fe086509d7caa8dec65e
dev-nicolaos <contact@nicolaos.dev>
2025-02-12 14:51:26 -0800

    rm duplicative slice sum test

--- slices/sum_test.go
+++ slices/sum_test.go
@@ -9,22 +9,10 @@ func assertMatch(t testing.TB, want, got int, input []int) {
 }
 
 func TestSum(t *testing.T) {
-	t.Run("Sums int array", func(t *testing.T) {
+	numbers := []int{1, 3, 5, 7}
 
-		numbers := []int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	want := 16
 
-		got := Sum(numbers)
-		want := 15
-
-		assertMatch(t, want, got, numbers)
-
-	})
-	t.Run("Sums an int slice", func(t *testing.T) {
-		numbers := []int{1, 3, 5, 7}
-
-		got := Sum(numbers)
-		want := 16
-
-		assertMatch(t, want, got, numbers)
-	})
+	assertMatch(t, want, got, numbers)
 }
