821f2d68562cc3abcfe9d8edb4af4c1379d468d6
dev-nicolaos <contact@nicolaos.dev>
2025-02-13 15:46:48 -0800

    Perimeter passing

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,5 +1,5 @@
 package structs
 
 func Perimeter(width, height float64) float64 {
-	return 0.0
+	return width*2 + height*2
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(2.0, 4.0)
-	want := 10.0
+	want := 12.0
 
 	if got != want {
 		t.Errorf("Expected %.2f, but got %.2f", want, got)
