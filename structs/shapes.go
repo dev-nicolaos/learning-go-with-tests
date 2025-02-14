0513bf48e7e0f79f8e3b0b2dbe76a71aba686a61
dev-nicolaos <contact@nicolaos.dev>
2025-02-13 16:09:33 -0800

    Methods for area and perimeter failing

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,14 +1,24 @@
 package structs
 
+// import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
+type Circle struct {
+	Radius  float64
+}
+
+func (rectangle *Rectangle) Perimeter() float64 {
 	return rectangle.Width*2 + rectangle.Height*2
 }
 
-func Area(rectangle Rectangle) float64 {
+func (rectangle *Rectangle) Area() float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+func (circle *Circle) Area() float64 {
+	return 0.0
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{2.0, 4.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 12.0
 
 	if got != want {
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{3.0, 6.0}
-	got := Area(rectangle)
-	want := 18.0
+	t.Run("for a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{3.0, 6.0}
+		got := rectangle.Area()
+		want := 18.0
 
-	if got != want {
-		t.Errorf("Expected %.2f, but got %.2f", want, got)
-	}
+		if got != want {
+			t.Errorf("Expected %g, but got %g", want, got)
+		}
+	})
+
+	t.Run("for a circle", func(t *testing.T) {
+		circle := Circle{4.5}
+		got := circle.Area()
+		want := 18.0
+
+		if got != want {
+			t.Errorf("Expected %g, but got %g", want, got)
+		}
+	})
 }
